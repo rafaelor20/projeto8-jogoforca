d0dd1cea55b7e901018a0a88caef1f31d45551a3
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2022-11-23 16:09:01 -0300

    css escritos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import palavras from "./palavras.js";
+
+const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 function App() {
   return (
